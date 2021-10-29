cb059775b3d7d6d9906c48ced890afff9a7ecac0
EricaMarroquin <ericamarroquin03@gmail.com>
2021-10-29 16:28:01 -0700

    add frontend UI

--- LocalBusiness/Models/LocalBusinessContext.cs
+++ LocalBusiness/Models/LocalBusinessContext.cs
@@ -8,7 +8,16 @@ namespace LocalBusiness.Models
     {
 
     }
-
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Business>()
+        .HasData(
+          new Business { BusinessId = 1, Name = "Viva", BusinessType = "Restaurant", Phone = "111-1111" },
+          new Business { BusinessId = 2, Name = "Panda Tea House", BusinessType = "Coffee/Tea", Phone = "222-2222" },
+          new Business { BusinessId = 3, Name = "Saigon", BusinessType = "Restaurant", Phone = "333-3333" },
+          new Business { BusinessId = 4, Name = "Fontanas", BusinessType = "Clothing", Phone = "444-4444" }
+        );
+    }
     public DbSet<Business> Businesses { get; set; }
   }
 }
