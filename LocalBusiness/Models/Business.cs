4b0e305705b6b1c7df7b30696976c23148476fc1
EricaMarroquin <ericamarroquin03@gmail.com>
2021-10-29 14:37:09 -0700

    WIP: adding autoincrementing unique IDs to business class

--- LocalBusiness/Models/Business.cs
+++ LocalBusiness/Models/Business.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LocalBusiness.Models
 {
   public class Business
   {
+    [Key]
     public int BusinessId { get; set; }
     public string Name { get; set; }
     public string BusinessType { get; set; }
